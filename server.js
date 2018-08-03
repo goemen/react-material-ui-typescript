85bfb5211729431517272f5235ffcfbf3243219a
Goeme Nthomiwa <gnthomiwa@live.ca>
2018-08-03 10:16:51 -0700

    remove whitespace

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
-
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
